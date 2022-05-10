510b64839bf47a88b14cfba91b871e3d98ffceeb
MargaritaMulkeeva <mulkeebarita2001@gmail.com>
2022-05-10 15:28:40 +0600

    Почти законченная верстка

---
+++ app/src/main/java/com/example/needlework/Discussions/AddDiscussions.java
@@ -0,0 +1,14 @@
+package com.example.needlework.Discussions;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddDiscussions extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_discussions);
+    }
+}
---
+++ app/src/main/java/com/example/needlework/Discussions/ChooseDiscussions.java
@@ -0,0 +1,14 @@
+package com.example.needlework.Discussions;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ChooseDiscussions extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_discussions);
+    }
+}
